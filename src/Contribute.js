6570d031735f30b3328b11bc94c88c87867a0a31
Aaron Barack <atbarack@gmail.com>
2021-05-06 08:55:00 +0900

    basic verify question format

--- src/Contribute.js
+++ src/Contribute.js
@@ -1,13 +1,51 @@
 import React from 'react';
 import './contribute.css';
-
-
+import { RiArrowGoBackFill } from "@react-icons/all-files/ri/RiArrowGoBackFill";
+import { FaCheck } from "@react-icons/all-files/fa/FaCheck";
+import { FaTimes } from "@react-icons/all-files/fa/FaTimes";
+//import { AiFillStar } from "@react-icons/all-files/ai/AiFillStar";
 export default function Contribute({setDisplay}){
 
   return(
     <div>
-      <h1>Add Questions</h1>
-      <button onClick={() => setDisplay(0)}>Go back</button>
+      <div id="header">
+        <button>Add</button>
+        <button>Verify</button>
+        <button id="back" onClick={() => setDisplay(0)}><RiArrowGoBackFill /></button>
+      </div>
+      <div id="verify">
+        <div id="question">
+          <div class="title">
+            What is the time and what is the meaning of life with the big pie in the sky?
+          </div>
+          <div class="option">
+            <div class="list"><FaTimes/></div>
+            <div class="content">A</div>
+          </div>
+          <div class="option">
+            <div class="list"><FaTimes/></div>
+            <div class="content">A</div>
+          </div>
+          <div class="option">
+            <div class="list"><FaCheck/></div>
+            <div class="content">A</div>
+          </div>
+          <div class="option">
+            <div class="list"><FaTimes/></div>
+            <div class="content">A</div>
+          </div>
+          <div class="choice">
+            <div class="a">
+              <FaCheck/>
+            </div>
+            <div class="b">
+              <FaTimes/>
+            </div>
+          </div>
+        </div>
+
+      </div>
+
     </div>
   );
 
